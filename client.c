810e067cd8e1c57966f9a3233a092708c58e9555
Peter Kim <peterkim95@ucla.edu>
2016-02-21 16:30:23 -0800

    asd

--- client.c
+++ client.c
@@ -1,70 +1,55 @@
 // client = receiver
-#include <stdio.h>
+/* UDP client in the internet domain */
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h>      // define structures like hostent
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 
-void error(char *msg)
+void error(const char *);
+int main(int argc, char *argv[])
+{
+   int sock, n;
+   unsigned int length;
+   struct sockaddr_in server, from;
+   struct hostent *hp;
+   char buffer[256];
+
+   if (argc != 3) { printf("Usage: server port\n");
+                    exit(1);
+   }
+   sock= socket(AF_INET, SOCK_DGRAM, 0);
+   if (sock < 0) error("socket");
+
+   server.sin_family = AF_INET;
+   hp = gethostbyname(argv[1]);
+   if (hp==0) error("Unknown host");
+
+   bcopy((char *)hp->h_addr,
+        (char *)&server.sin_addr,
+         hp->h_length);
+   server.sin_port = htons(atoi(argv[2]));
+   length=sizeof(struct sockaddr_in);
+   printf("Please enter the message: ");
+   bzero(buffer,256);
+   fgets(buffer,255,stdin);
+   n=sendto(sock,buffer,
+            strlen(buffer),0,(const struct sockaddr *)&server,length);
+   if (n < 0) error("Sendto");
+   n = recvfrom(sock,buffer,256,0,(struct sockaddr *)&from, &length);
+   if (n < 0) error("recvfrom");
+   write(1,"Got an ack: ",12);
+   write(1,buffer,n);
+   close(sock);
+   return 0;
+}
+
+void error(const char *msg)
 {
     perror(msg);
     exit(0);
-}
-
-int main(int argc, char *argv[])
-{
-    int sockfd; //Socket descriptor
-    int portno, n;
-    char* filename;
-    struct sockaddr_in serv_addr;
-    struct hostent *server; //contains tons of information, including the server's IP address
-
-    char buffer[256];
-    if (argc < 4) {
-       fprintf(stderr,"usage %s hostname port filename\n", argv[0]);
-       exit(0);
-    }
-
-    // Set file name
-    filename = argv[3];
-    printf("FN: %s\n", filename);
-
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0); //create a new socket
-    if (sockfd < 0)
-        error("ERROR opening socket");
-
-    server = gethostbyname(argv[1]); //takes a string like "www.yahoo.com", and returns a struct hostent which contains information, as IP address, address type, the length of the addresses...
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
-
-    memset((char *) &serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET; //initialize server's address
-    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-    serv_addr.sin_port = htons(portno);
-
-    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) //establish a connection to the server
-        error("ERROR connecting");
-
-    printf("Please enter the message: ");
-    memset(buffer,0, 256);
-    fgets(buffer,255,stdin);	//read message
-
-    n = write(sockfd,buffer,strlen(buffer)); //write to the socket
-    if (n < 0)
-         error("ERROR writing to socket");
-
-    memset(buffer,0,256);
-    n = read(sockfd,buffer,255); //read from the socket
-    if (n < 0)
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);	//print server's response
-
-    close(sockfd); //close socket
-
-    return 0;
 }

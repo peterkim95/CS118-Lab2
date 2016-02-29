07feb9979451d02b21658c66508a1e0809750e13
Peter Kim <peterkim95@ucla.edu>
2016-02-28 17:47:34 -0800

    change packet structure and how to add delay timeout for each packet??

--- client.c
+++ client.c
@@ -11,79 +11,73 @@
 #include <string.h>
 
 #define HEADER_SIZE 64
-#define PACKET_SIZE 1024
+#define PACKET_DATA_SIZE 1008 // 1024 - 4(INT) = 1024 - (4*4) = 1008
 
-void error(const char *);
-int main(int argc, char *argv[])
-{
-   int sock, n;
-   unsigned int length;
-   struct sockaddr_in server, from;
-   struct hostent *hp;
-   char buffer[1024];
-   char* filename;
+struct packet {
+  int type;	// 0: Request, 1: Data, 2: ACK, 3: FIN
+  int seq;	// Packet sequence number
+  int size;	// Data size
+  int checksum;   // To detect corruption
 
-   if (argc != 4) {
-     printf("Usage: ./client host port filename\n");
-     exit(1);
-   }
-
-   // Get requested filename
-   filename = argv[3];
-
-   sock = socket(AF_INET, SOCK_DGRAM, 0);
-   if (sock < 0) error("socket");
-   server.sin_family = AF_INET;
-
-   // Localhost support
-   if (strncmp(argv[1],"localhost", strlen(argv[1])) == 0)
-     hp = gethostbyname("127.0.0.1");
-   else
-     hp = gethostbyname(argv[1]);
-   if (hp == 0) error("Unknown host");
-
-   bcopy((char *)hp->h_addr, (char *)&server.sin_addr, hp->h_length);
-   server.sin_port = htons(atoi(argv[2]));
-   length=sizeof(struct sockaddr_in);
-
-   bzero(buffer,PACKET_SIZE);
-
-   //memcpy(buffer, filename, strlen(filename) + 1);
-
-   char header[HEADER_SIZE];
-   // construct header here
-
-   // PACKET STRUCTURE
-   /*
-
-
-    int type;	// 0: Initial File Request, 1: Data, 2: ACK, 3: FIN (signal for final packet)
-    int seq;	// Packet sequence number
-    int size;	// Data size
-    int checksum; // For detecting corruption
-
-    char* data;
-   */
-
-   memcpy(buffer, header, HEADER_SIZE);
-
-   memcpy(buffer + HEADER_SIZE, filename, strlen(filename) + 1);
-
-
-   n = sendto(sock, buffer, PACKET_SIZE, 0, (const struct sockaddr*)&server, length);
-   if (n < 0) error("Sendto");
-   n = recvfrom(sock,buffer,256,0,(struct sockaddr *)&from, &length);
-   if (n < 0) error("recvfrom");
-   write(1,"Got an ack: ",12);
-
-   write(1,buffer,n);
-
-   close(sock);
-   return 0;
-}
+  char data[PACKET_DATA_SIZE];
+};
 
 void error(const char *msg)
 {
-    perror(msg);
-    exit(0);
+  perror(msg);
+  exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+  int sock, n;
+  unsigned int length;
+  struct packet incoming, outgoing;
+  struct sockaddr_in server, from;
+  struct hostent *hp;
+  char buffer[1024];
+  char* filename;
+
+  if (argc != 4)
+  {
+    printf("Usage: ./client host port filename\n");
+    exit(1);
+  }
+
+  // Get requested filename
+  filename = argv[3];
+
+  sock = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sock < 0) error("socket");
+  server.sin_family = AF_INET;
+
+  // Localhost support
+  if (strncmp(argv[1],"localhost", strlen(argv[1])) == 0)
+    hp = gethostbyname("127.0.0.1");
+  else
+    hp = gethostbyname(argv[1]);
+  if (hp == 0) error("Unknown host");
+
+  bcopy((char *)hp->h_addr, (char *)&server.sin_addr, hp->h_length);
+  server.sin_port = htons(atoi(argv[2]));
+  length=sizeof(struct sockaddr_in);
+
+  bzero((char *) &outgoing, sizeof(outgoing));
+  outgoing.type = 0;
+  outgoing.seq = 0;
+  outgoing.size = strlen(filename) + 1;
+  strcpy(outgoing.data, filename);
+
+
+  n = sendto(sock, &outgoing, sizeof(outgoing), 0, (const struct sockaddr*)&server, length);
+  if (n < 0) error("Sendto");
+
+  n = recvfrom(sock,buffer,256,0,(struct sockaddr *)&from, &length);
+  if (n < 0) error("recvfrom");
+  write(1,"Got an ack: ",12);
+
+  write(1,buffer,n);
+
+  close(sock);
+  return 0;
 }
--- server.c
+++ server.c
@@ -13,98 +13,125 @@
 #include <stdio.h>
 
 #define HEADER_SIZE 64
-#define PACKET_SIZE 1024
+#define PACKET_DATA_SIZE 1008 // 1024 - 4(INT) = 1024 - (4*4) = 1008
+
+struct packet {
+  int type;	// 0: Request, 1: Data, 2: ACK, 3: FIN
+  int seq;	// Packet sequence number
+  int size;	// Data size
+  int checksum;   // To detect corruption
+
+  char data[PACKET_DATA_SIZE];
+};
 
 void error(const char *msg)
 {
-    perror(msg);
-    exit(0);
+  perror(msg);
+  exit(0);
 }
 
 int main(int argc, char *argv[])
 {
-   int sock, length, n;
-   socklen_t fromlen;
-   struct sockaddr_in server;
-   struct sockaddr_in from;
-   char buf[1024];
+  int sock, length, n;
+  socklen_t clientlen;
+  struct sockaddr_in server, client;
 
-   if (argc < 2) {
-      fprintf(stderr, "ERROR, no port provided\n");
-      exit(0);
-   }
+  struct packet incoming, outgoing;
 
-   sock=socket(AF_INET, SOCK_DGRAM, 0);
-   if (sock < 0) error("Opening socket");
-   length = sizeof(server);
-   bzero(&server,length);
-   server.sin_family=AF_INET;
-   server.sin_addr.s_addr=INADDR_ANY;
-   server.sin_port=htons(atoi(argv[1]));
-   if (bind(sock,(struct sockaddr *)&server,length)<0)
-       error("binding");
-   fromlen = sizeof(struct sockaddr_in);
+  char buf[1024];
 
-   while (1) {
-       n = recvfrom(sock,buf,1024,0,(struct sockaddr *)&from,&fromlen);
+  if (argc < 2) {
+    fprintf(stderr, "ERROR, no port provided\n");
+    exit(0);
+  }
 
-       if (n < 0) error("recvfrom");
+  sock=socket(AF_INET, SOCK_DGRAM, 0);
+  if (sock < 0) error("Opening socket");
+  length = sizeof(server);
+  bzero(&server,length);
+  server.sin_family=AF_INET;
+  server.sin_addr.s_addr=INADDR_ANY;
+  server.sin_port=htons(atoi(argv[1]));
+  if (bind(sock, (struct sockaddr *)&server, length) < 0)
+    error("binding");
+  clientlen = sizeof(struct sockaddr_in);
 
-       write(1,"Received a datagram: ",21);
+  while (1)
+  {
+    n = recvfrom(sock,&incoming, sizeof(incoming),0,(struct sockaddr *)&client,&clientlen);
 
-       // Parse packet into header and data
-       // char header[HEADER_SIZE];
+    if (n < 0) error("recvfrom");
 
-       char* filename;
-       filename = buf+HEADER_SIZE;
-
-       printf("filename=%s\n", buf+HEADER_SIZE);
-
-       // Check if file exists
-       FILE *fp = fopen(filename, "r");
-       if (!fp) error("Requested file doesn't exist in the working directory");
-
-       // Figure out file size
-       fseek(fp, 0L, SEEK_END);
-       int fsize = (int) ftell(fp);
-       fseek(fp, 0L, SEEK_SET);
-
-       printf("file size = %d\n", fsize);
-
-       int total;
-    	 total = fsize / PACKET_SIZE;
-    	 if (fsize % PACKET_SIZE > 0)
-    	   total++;
-    	 printf("Required packets: %d\n", total);
-
-       int wsize = 5;
-       int wstart = 0;
-
-       int cur = 0;
-
-       while (cur < total)
-       {
-         int wend = wstart + wsize;
-         while (cur < wend)
-         {
-           send cur;
-           cur++;
-
-         }
-       }
-
-       // Divide file into packets
-
-       // Send each packet as they are ready
-
-       //write(1, buf + HEADER_SIZE, PACKET_SIZE - HEADER_SIZE);
-       // printf ("DATA: %s", buf + HEADER_SIZE);
-       //write(1,buf+HEADER_SIZE,n);
+    write(1,"Received a datagram: ",21);
 
 
-       n = sendto(sock,"Got your message\n",17, 0,(struct sockaddr *)&from,fromlen);
-       if (n  < 0) error("sendto");
-   }
 
-   return 0;
+    char* filename;
+    filename = incoming.data;
+
+    printf("filename=%s\n", filename);
+
+    // Check if file exists
+    FILE *fp = fopen(filename, "r");
+    if (!fp) error("ERROR - Requested file doesn't exist in the working directory!");
+
+    // Figure out file size
+    fseek(fp, 0L, SEEK_END);
+    int fsize = (int) ftell(fp);
+    fseek(fp, 0L, SEEK_SET);
+
+    printf("file size = %d\n", fsize);
+
+    int total;
+    total = fsize / 1024;
+    if (fsize % 1024 > 0)
+      total++;
+    printf("Required packets: %d\n", total);
+
+    int wsize = 5;
+    int wstart = 0;
+
+    int cur = 0;
+
+    fd_set readset;
+    struct timeval timeout = {1, 0};   // 1 sec timeout
+
+    while (cur < total)
+    {
+      int wend = wstart + wsize;
+      while (cur < wend)
+      {
+        bzero((char *) &outgoing, sizeof(outgoing));
+        outgoing.type = 1; // data packet
+        outgoing.seq = wstart + cur;
+        fseek(fp, outgoing.seq * PACKET_DATA_SIZE, SEEK_SET);
+        outgoing.size = fread(outgoing.data, 1, PACKET_DATA_SIZE, fp);
+
+        // send next packet in window
+        if (sendto(sock, &outgoing, sizeof(outgoing), 0, (struct sockaddr*) &client, clientlen) < 0)
+        	error("ERROR sending packet\n");
+        cur++;
+
+        // HOW TO ATTACH A TIMER ON EACH PACKET?????
+
+
+
+      }
+    }
+
+    // Divide file into packets
+
+    // Send each packet as they are ready
+
+    //write(1, buf + HEADER_SIZE, PACKET_SIZE - HEADER_SIZE);
+    // printf ("DATA: %s", buf + HEADER_SIZE);
+    //write(1,buf+HEADER_SIZE,n);
+
+
+    n = sendto(sock,"Got your message\n",17, 0,(struct sockaddr *)&client,clientlen);
+    if (n  < 0) error("sendto");
+
+  }
+
+  return 0;
 }
